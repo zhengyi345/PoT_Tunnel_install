ea1b573a208ae9649be83fe0a176fe649126f89f
zhengyi345 <31331632+zhengyi345@users.noreply.github.com>
2019-12-02 23:32:44 +0800

    Update pot-tunnel-install.sh

--- pot-tunnel-install.sh
+++ pot-tunnel-install.sh
@@ -4,8 +4,7 @@
 
 echo “开始执行PoT隧道脚本安装...”
 wget  -N --no-check-certificate https://dl.google.com/go/go1.13.4.linux-amd64.tar.gz && tar -C /usr/local -xzf go1.13.4.linux-amd64.tar.gz && echo -n "export PATH=$PATH:/usr/local/go/bin" >> /etc/profile && source /etc/profile
-git clone https://github.com/goproxyio/goproxy.git 
-cd goproxy && make
+curl -L https://raw.githubusercontent.com/snail007/goproxy/master/install_auto.sh | bash
 export GO111MODULE=on
 go env -w GOPROXY=https://goproxy.cn,direct
 
